8f2b441854a502092e2f97844ccc455063fb8435
Dmitry Shevchenko <shevchen239@gmail.com>
2011-12-03 03:54:36 +0400

    Another fucking inattention.

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -25,14 +25,14 @@ public class Constants {
 			1. };
 
 	public static final double[] BEST_MUTATION_PROBABILITIES = { 0.5, 1.0, 0.7,
-			1.0 };
+			0.7 };
 
 	public static final MersenneTwister rand = new MersenneTwister(System
 			.nanoTime());
 
 	public static final int THREADS = 2;
 	public static final int RUNNINGS_PER_THREAD = 5;
-	public static final int SEARCHES_PER_THREAD = 1;
+	public static final int SEARCHES_PER_THREAD = 5;
 
 	public static final Paint[] DEFAULT_COLORS = { Color.RED, Color.GREEN,
 			Color.BLUE, Color.BLACK, new Color(250, 127, 50) /* bronze */,
