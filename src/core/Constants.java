a58ba137b2dcd23550ce30d938fbae25bc00cc07
Dmitry Shevchenko <shevchen239@gmail.com>
2012-01-06 18:51:07 +0400

    Need more data.

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -31,8 +31,8 @@ public class Constants {
 			.nanoTime());
 
 	public static final int THREADS = 2;
-	public static final int RUNNINGS_PER_THREAD = 15;
-	public static final int SEARCHES_PER_THREAD = 10;
+	public static final int RUNNINGS_PER_THREAD = 50;
+	public static final int SEARCHES_PER_THREAD = 50;
 
 	public static final Paint[] DEFAULT_COLORS = { Color.RED, Color.GREEN,
 			Color.BLUE, Color.BLACK, new Color(250, 127, 50) /* bronze */,
