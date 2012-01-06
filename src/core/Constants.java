1728da1101e1dfe5eb335dfebb3b9e22edb4dbd7
Dmitry Shevchenko <shevchen239@gmail.com>
2012-01-06 07:44:37 +0400

    Trash detected.

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
 	public static final int FIELDS_IN_GENERATION = 50;
 	public static final int ITERATIONS = 2000;
 	public static final int SEARCHER_ITERATIONS = 2000;
-	public static final int STAGNATION_TIME = 500;
+	public static final int STAGNATION_TIME = 300;
 	public static final double STAGNATION_DELTA = 0.001;
 	public static final double NO_STAGNATION_THRESHOLD = 0.4;
 
@@ -34,8 +34,8 @@ public class Constants {
 			.nanoTime());
 
 	public static final int THREADS = 2;
-	public static final int RUNNINGS_PER_THREAD = 10;
-	public static final int SEARCHES_PER_THREAD = 10;
+	public static final int RUNNINGS_PER_THREAD = 25;
+	public static final int SEARCHES_PER_THREAD = 20;
 
 	public static final Paint[] DEFAULT_COLORS = { Color.RED, Color.GREEN,
 			Color.BLUE, Color.BLACK, new Color(250, 127, 50) /* bronze */,
