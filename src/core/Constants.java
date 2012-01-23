b1d230b51ba4c715dcab724c45ea2cbf2f130b65
Dmitry Shevchenko <shevchen239@gmail.com>
2012-01-24 01:38:48 +0400

    Actual launch number.

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -31,8 +31,8 @@ public class Constants {
 			.nanoTime());
 
 	public static final int THREADS = 2;
-	public static final int RUNNINGS_PER_THREAD = 25;
-	public static final int SEARCHES_PER_THREAD = 25;
+	public static final int RUNNINGS_PER_THREAD = 50;
+	public static final int SEARCHES_PER_THREAD = 50;
 
 	public static final Paint[] DEFAULT_COLORS = { Color.RED, Color.GREEN,
 			Color.BLUE, Color.BLACK, new Color(250, 127, 50) /* bronze */,
